7622388b94febd3813aac81f04eccf8673817262
Ajay Pratap Rav <98567614+ajaypratap5301@users.noreply.github.com>
2024-04-14 20:05:55 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,1 +1,11 @@
+create database employeedb;
+
+CREATE TABLE Employee (
+    eid INT PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    phone VARCHAR(15),
+    department VARCHAR(50),
+    salary INT
+);
 
